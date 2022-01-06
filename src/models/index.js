da27c2c65785f1a4caaac7e37c18cc867e3c4d9d
Brenno Pereira Rodrigues <brennopereira6@gmail.com>
2022-01-06 17:33:40 -0300

    removed useless database.js

--- src/models/index.js
+++ src/models/index.js
@@ -11,6 +11,12 @@ const db = {};
 let sequelize;
 sequelize = new Sequelize(config.database, config.username, config.password, config);
 
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch(error => {
+  console.error('Unable to connect to the database:', error);
+});
+
 fs
   .readdirSync(__dirname)
   .filter(file => {
